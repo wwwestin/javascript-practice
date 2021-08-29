34b44e43ccb12af4999e03628aec546ddee5b10f
Westin Humble <westinhumble@westins-mbp.myfiosgateway.com>
2021-08-29 15:31:02 -0400

    initial commit

---
+++ lecture/expressions.js
@@ -0,0 +1,13 @@
+//console.log(1 + 1);
+//console.log(2*8);
+//console.log(12/3);
+//console.log(100-1);
+//console.log(1+1-5);
+//console.log((1+2)*5);
+//console.log(14 % 5);
+//console.log(15 % 5);
+//console.log(13 % 6);
+//console.log(4 % 10);
+//console.log("westin328746287351");
+//console.log("cat"+"dog");
+console.log("cat".toUpperCase());
---
+++ my-first-code/my-first-program.js
@@ -0,0 +1,4 @@
+// this is my first program, console.log will print something to the screen
+console.log("hello!!");
+// console.log("goodbye everyone!");
+// console.log("whoa");
