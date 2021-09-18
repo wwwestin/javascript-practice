1e891b2bca520f0db4d7c26f5b70f29168a2233e
Westin Humble <westinhumble@westins-mbp.myfiosgateway.com>
2021-09-18 16:10:02 -0400

    added conditionals exercises

---
+++ conditional_exercise/zero.js
@@ -0,0 +1,52 @@
+//snipppet 0-1
+if (true) {
+    console.log("foo");
+}
+
+if (false){
+    console.log("bar");
+}
+
+//snippet 0-2
+if (false|| false){
+    console.log("boop");
+}
+
+if (true || false){
+    console.log("beep");
+}
+
+// snippet 0-3
+let num= 40;
+
+if (num > 0) {
+    console.log("zip");
+}
+
+if (num % 2 === 0){
+    console.log("zoop");
+}
+
+// snippet 0-4
+let word = "jeep";
+
+if (word[0] === "d"){
+    console.log("yer");
+} else {
+    console.log("nah");
+}
+
+// snippt 0-5
+let sentence = "roger that";
+
+if (sentence[sentence.length - 1] === "t"){
+    console.log("ends in t");
+} else {
+    console.log("does not end in t");
+}
+
+if (sentence.length <= 4){
+    console.log("short");
+} else {
+    console.log("long");
+}
