2fab619610bf46d5d8ddd1280319e2103c6fc4b9
westin humble <westin.h@gmail.com>
2021-10-26 16:08:39 -0400

    loops exercises part one attempt 1

---
+++ A_loops_exercise/snippet-2.js
@@ -0,0 +1,8 @@
+console.log("hi");
+
+for (let i = 3; i <= 7; i++) {
+    console.log("program");
+    console.log(i);
+}
+
+console.log("bye");
---
+++ A_loops_exercise/snippet-5.js
@@ -0,0 +1,6 @@
+let total = 0;
+for (let i = 1; i < 5; i++) {
+    total += i;
+    console.log(total);
+}
+
