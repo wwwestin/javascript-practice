2f2bf8e3cdc84bfcfe829b87c77e07de105f97c8
westin humble <westin.h@gmail.com>
2021-10-26 15:23:58 -0400

    notes on loops lecture

---
+++ lecturing/Loops.js
@@ -0,0 +1,41 @@
+// // count = 0, runs according the count farthest to left, count ends with count in the middle.
+//for (let count = 1; count <= 19; count++) {
+  // console.log('I am hungry');
+//}
+// single cycle of loop = iteration
+
+//for (let i = 10; i >= 0; i--){
+  //  console.log(i);
+  //  console.log('loop');
+//}
+
+//let str = 'potato';
+
+
+//for (let i = 0; i < str.length; i++){
+  // console.log(str[i]);
+//}
+
+//let str = 'geeb is cute';
+
+//for (let i = 0; i < str.length; i++) {
+ //   console.log(str[i]);
+ //   console.log('---');
+//}
+
+//let sum = 0;
+
+//for (let num = 1; num <= 3; num++) {
+ //   sum += num;
+//}
+
+//console.log(sum);
+
+for (let num = 0; num <= 10; num++){
+    console.log(num)
+    if(num % 2 === 0) {
+        console.log('it is even');
+    } else {
+        console.log('it is odd')
+    }
+}
