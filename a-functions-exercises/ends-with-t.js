f8cafd3705bb2da020e74d193d6f4990972478fa
westin humble <westin.h@gmail.com>
2021-11-01 12:42:57 -0400

    functions exercises part 1 attempt 3

---
+++ a-functions-exercises/ends-with-t.js
@@ -0,0 +1,9 @@
+let endsWithT = function (str) {
+    let lastChar = str[str.length - 1]; 
+        return lastChar === 't'; 
+};
+
+console.log(endsWithT("smart")); // true
+console.log(endsWithT("racket")); // true
+console.log(endsWithT("taco")); // false
+console.log(endsWithT("boomerang")); // false
---
+++ a-functions-exercises/is-div-by-4.js
@@ -0,0 +1,9 @@
+let isDivBy4 = function (num) {
+  return (num % 4 === 0);
+};
+
+console.log(isDivBy4(8)); // true
+console.log(isDivBy4(12)); // true
+console.log(isDivBy4(24)); // true
+console.log(isDivBy4(9)); // false
+console.log(isDivBy4(10)); // false
