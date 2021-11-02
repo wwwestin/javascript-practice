f45a55eb0c148fa32230ea3ebdc4d1de8ba16a9d
westin humble <westin.h@gmail.com>
2021-11-02 11:01:02 -0400

    loops exercises part 2 attempt 2

---
+++ B-loops-exercise/div-by-either.js
@@ -0,0 +1,25 @@
+let divByEither = function (num1, num2, max) {
+    for (let i = 1; i < max; i++) {
+        if (i % num1 === 0 || i % num2 === 0) {
+            console.log(i);
+        }
+    }
+};
+
+divByEither(4, 3, 16);
+// prints
+//  3
+//  4
+//  6
+//  8
+//  9
+//  12
+//  15
+
+divByEither(7, 5, 20);
+// prints
+//  5
+//  7
+//  10
+//  14
+//  15
