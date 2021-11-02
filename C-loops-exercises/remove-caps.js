90245e9bfccf8501846bbfa4115168869523c04d
westin humble <westin.h@gmail.com>
2021-11-02 13:51:37 -0400

    loops exercises part 3 attempt 2

---
+++ C-loops-exercises/remove-caps.js
@@ -0,0 +1,17 @@
+let removeCapitals = function (str){
+    let newStr= '';
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === char.toLowerCase()) {
+            newStr += char;
+        }
+    }
+    
+    return newStr;
+};
+
+
+console.log(removeCapitals("fOrEver")); // 'frver'
+console.log(removeCapitals("raiNCoat")); // 'raioat'
+console.log(removeCapitals("cElLAr Door")); // 'clr oor'
