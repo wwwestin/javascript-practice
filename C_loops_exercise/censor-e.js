0e160caa5a4570eba0a4f46e849ff5929a7e93b3
westin humble <westin.h@gmail.com>
2021-11-03 14:55:33 -0400

    loops exercises part 3 attempt 3

---
+++ C_loops_exercise/censor-e.js
@@ -0,0 +1,19 @@
+let censorE = function (str) {
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === 'e') {
+            newStr += "*";
+        } else {
+            newStr += char;
+        }
+
+    }
+    return newStr;
+};
+
+console.log(censorE("speedy")); // 'sp**dy'
+console.log(censorE("pending")); // 'p*nding'
+console.log(censorE("scene")); // 'sc*n*'
+console.log(censorE("heat")); // 'h*at'
