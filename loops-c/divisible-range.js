955c7f4eee33c95ee2020fd17ac904d90557bd2a
westin humble <westin.h@gmail.com>
2021-11-04 15:50:26 -0400

    loops exercises part 3 attempt 5

---
+++ loops-c/divisible-range.js
@@ -0,0 +1,20 @@
+let divisibleRange = function (min, max, num) {
+
+    for (let i = min; i < max; i++) {
+        if (i % num === 0) {
+            console.log(i);
+        }
+    }
+};
+
+divisibleRange(17, 40, 9);
+// prints
+//  18
+//  27
+//  36
+
+divisibleRange(10, 24, 4);
+// prints
+//  12
+//  16
+//  20
